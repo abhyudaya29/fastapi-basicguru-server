174d6bbf938e3a22551236488fc9cb1f264cff32
abhyudaya29 <abhyudayadev123@gamil.com>
2025-04-15 23:20:09 +0530

    user models

--- models/usermodels.py
+++ models/usermodels.py
@@ -1,6 +1,9 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Enum,Column, Integer, String
 from database import Base
-
+import enum
+class UserRole(str, enum.Enum):
+    admin = "admin"
+    student = "student"
 class User(Base):
     __tablename__="users"
     id=Column(Integer,primary_key=True,index=True)
@@ -8,3 +11,4 @@ class User(Base):
     email=Column(String,unique=True,index=True)
     phone=Column(String,unique=True,index=True)
     password=Column(String)
+    role=Column(Enum(UserRole),default=UserRole.student,nullable=False)
